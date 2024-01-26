19c3ea9d4b56b3242a01736ff4c8dcafbcc0c065
F Jiang <jiang.tf.2020@gmail.com>
2024-01-26 14:17:33 +0000

    Update train output

--- scripts/slurm-train.sh
+++ scripts/slurm-train.sh
@@ -17,4 +17,4 @@ source activate
 # slurm-train
 # ==============================================================================
 
-python -m train --model_save_path model_augmented.pt
+python -m train --model_save_path model_parole.pt
